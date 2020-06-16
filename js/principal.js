e6a3b19f07e0a7b898be4a2038d4086caeb60160
Vinicius Lago Gehrke <vinictus@gmail.com>
2020-06-16 22:23:21 +0000

    Using querySelector to search values on table. Calculates IMC using logic operators (if, else, ||, &&)

---
+++ js/principal.js
@@ -0,0 +1,31 @@
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+var tdImc = paciente.querySelector(".info-imc");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+//validar altura e peso inseridos
+var alturaEhValida = true;
+var pesoEhValido = true;
+
+if(peso <= 0 || peso > 1000){
+    console.log("Peso inválido");
+    tdImc.textContent = "Peso inválido!";
+    pesoEhValido = false;
+}
+
+if(altura <= 0 || altura >= 3){
+    console.log("Altura inválida");
+    tdImc.textContent = "Altura inválida!";
+    alturaEhValida = false;
+}
+
+if(pesoEhValido && alturaEhValida){
+    var imc = peso / ( altura * altura);    
+    tdImc.textContent = imc;
+} else{
+    tdImc.textContent = "Altura e/ou peso inválidos!"
+}
