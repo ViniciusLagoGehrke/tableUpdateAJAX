f7ed223f8fe875ebc06441936387f72c29670a7b
Vinicius Lago Gehrke <46430040+ViniciusLagoGehrke@users.noreply.github.com>
2020-06-17 01:34:38 +0100

    Add files via upload
    same as previous commit

--- js/form.js
+++ js/form.js
@@ -73,29 +73,27 @@ function exibeMensagensDeErro(erros) {
     });
 }
 
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
+
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
 botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
-    
     var form = document.querySelector("#form-adiciona");
     var paciente = obtemPacienteDoFormulario(form);
-    
     var erros = validaPaciente(paciente);
-
     if (erros.length > 0) {
-      exibeMensagensDeErro(erros);
+        exibeMensagensDeErro(erros);
         return;
     }
-    
-    var pacienteTr = montaTr(paciente);
-    
-    var tabela = document.querySelector("#tabela-pacientes");
 
-    tabela.appendChild(pacienteTr);
+    adicionaPacienteNaTabela(paciente);
 
-    form.reset();
-
+    form.reset();    
     var mensagensErro = document.querySelector("#mensagens-erro");
     mensagensErro.innerHTML = "";
 });
